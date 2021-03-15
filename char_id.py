e824b27ddc6864c962f6ccdf60a72619555cc179
yashgautam06 <80680021+yashgautam06@users.noreply.github.com>
2021-03-15 15:36:04 +0530

    its my second code

---
+++ char_id.py
@@ -0,0 +1,10 @@
+n= input()
+if n>='A' and n<='Z':
+    print("upper case")
+elif n>='a' and n<='z' :
+    print("Lower case")
+elif n>='0' and n<='9 :
+    print("Digit")
+else :
+    print("Special character")
+   
