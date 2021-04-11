9d6d669fd65949e124ef27bfc51bc836397007f4
yashgautam06 <80680021+yashgautam06@users.noreply.github.com>
2021-04-11 21:17:17 +0530

    Add files via upload

---
+++ search functions in pythonpy..py
@@ -0,0 +1,4 @@
+In python ,search() is a method of the module re.
+synatx of search ()
+re.search(pattern,string):
+    It is similar to re.match()but it does not limit us to find matches at the beginning of the string only.
